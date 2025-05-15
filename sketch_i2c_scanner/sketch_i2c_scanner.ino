0cd5b56466baf0cdccfdc02a4b532da1b661a37b
Ossi Koski <ossi.koski@wapice.com>
2025-05-15 22:01:37 +0300

    Debugging for particle sensor

--- sketch_i2c_scanner/sketch_i2c_scanner.ino
+++ sketch_i2c_scanner/sketch_i2c_scanner.ino
@@ -1,29 +1,19 @@
+
 #include <Wire.h>
 
 void setup() {
-  Wire.begin(19, 20); // your SDA and SCL
+  Wire.begin(19, 20); // SDA, SCL
   Serial.begin(115200);
-  while (!Serial);
-  delay(1000);
-  Serial.println("I2C scanner running...");
+  delay(3000);
+  Serial.println("Scanning I2C bus...");
 
-  byte error, address;
-  int nDevices = 0;
-
-  for(address = 1; address < 127; address++ ) {
+  for (uint8_t address = 1; address < 127; address++) {
     Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-
-    if (error == 0) {
-      Serial.print("I2C device found at 0x");
+    if (Wire.endTransmission() == 0) {
+      Serial.print("Found I2C device at 0x");
       Serial.println(address, HEX);
-      nDevices++;
     }
   }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found.\n");
-  else
-    Serial.println("Done.\n");
 }
 
 void loop() {}
